7a35ec8a3ae1e07ca349807ea3ebd52dfad4a0e0
unknown <andy@.(none)>
2012-09-21 19:28:10 +0200

    delete installer
    delete installer

--- zpanelx-installers/windows/bin/mysql/mysql-test/suite/rpl/t/rpl_loaddata_symlink-slave.sh
+++
@@ -1,2 +0,0 @@
-rm -f $MYSQLTEST_VARDIR/std_data_slave_link
-ln -s $MYSQLTEST_VARDIR/std_data $MYSQLTEST_VARDIR/std_data_slave_link
--- zpanelx-installers/windows/bin/zpss/MySQL_User_Cleanup.sql
+++
@@ -1,3 +0,0 @@
-USE mysql;
-DELETE FROM user WHERE User = "" OR Host = "127.0.0.1" OR Host = "::1";
-FLUSH PRIVILEGES;
