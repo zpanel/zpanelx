7b5c9d27fe14f2659965e04c9b5335acc9b5de63
Kimpe Andy <andykimpe@gmail.com>
2013-12-25 03:34:54 +0100

    Update zpanel_update.sh

--- etc/build/config_packs/centos_6_3/zpanelx-update/10-0-2/shell/zpanel_update.sh
+++ etc/build/config_packs/centos_6_3/zpanelx-update/10-0-2/shell/zpanel_update.sh
@@ -11,6 +11,20 @@ genpasswd() {
 zadminNewPass=`genpasswd`
 setzadmin --set $zadminNewPass
 
+# update translation
+mysqlrootpass=`cat /root/mysqlrootpass`
+checktranslation=`mysql -u root -p$mysqlrootpass -e "SELECT COUNT(*) FROM zpanel_core.x_translations;" | grep "828"`
+translationline="828"
+if [ "$checktranslation" == "$translationline" ]
+then
+echo ""
+else
+echo "update translation"
+wget -q https://github.com/ZPanelFR/zpxfrtrad/raw/master/uninstall.sql
+mysql -u root -p$mysqlrootpass < uninstall.sql
+rm -f uninstall.sql
+fi
+
 ##BUILD RETURN MESSAGE##
 updatemessages=""
 updatemessages="$updatemessages zadmin password has been updated to : $zadminNewPass\n"
